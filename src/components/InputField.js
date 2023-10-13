dbb72fb84e8fa7ef65f559c784f32f839c13f659
Ricardo Contreras <ricardo.contreras@hermosillo.com>
2023-10-13 15:57:14 -0700

    Fix number format

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -4,7 +4,7 @@ import Textarea from 'react-textarea-autosize';
 import {Icon, Progress} from "semantic-ui-react";
 import Cleave from 'cleave.js/react';
 import InputConfirm from "./InputConfirm";
-import NumberFormat from 'react-number-format';
+import {default as NumberFormat} from 'react-number-format';
 import dateFormatter from "../utils/dateFormatter";
 
 
