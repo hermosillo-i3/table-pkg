1b60fff00ce331061c853dd746434c3ca2b143b7
Ricardo Contreras <ricardo.contreras@hermosillo.com>
2023-10-24 17:34:29 -0700

    Fix drag and drop styles

--- src/components/Row.js
+++ src/components/Row.js
@@ -9,6 +9,7 @@ import {
    useDrag, 
    useDrop, 
    // connectDragPreview
+   useDragDropManager,
 } from 'react-dnd'
 import {isColumnEditable} from "../utils/column";
 
@@ -17,41 +18,6 @@ const faGripVertical = {
    iconName: 'grip-vertical',
    icon: [320, 512, [], "f58e", "M40 352l48 0c22.1 0 40 17.9 40 40l0 48c0 22.1-17.9 40-40 40l-48 0c-22.1 0-40-17.9-40-40l0-48c0-22.1 17.9-40 40-40zm192 0l48 0c22.1 0 40 17.9 40 40l0 48c0 22.1-17.9 40-40 40l-48 0c-22.1 0-40-17.9-40-40l0-48c0-22.1 17.9-40 40-40zM40 320c-22.1 0-40-17.9-40-40l0-48c0-22.1 17.9-40 40-40l48 0c22.1 0 40 17.9 40 40l0 48c0 22.1-17.9 40-40 40l-48 0zM232 192l48 0c22.1 0 40 17.9 40 40l0 48c0 22.1-17.9 40-40 40l-48 0c-22.1 0-40-17.9-40-40l0-48c0-22.1 17.9-40 40-40zM40 160c-22.1 0-40-17.9-40-40L0 72C0 49.9 17.9 32 40 32l48 0c22.1 0 40 17.9 40 40l0 48c0 22.1-17.9 40-40 40l-48 0zM232 32l48 0c22.1 0 40 17.9 40 40l0 48c0 22.1-17.9 40-40 40l-48 0c-22.1 0-40-17.9-40-40l0-48c0-22.1 17.9-40 40-40z"]
 };
-
-
-const RowTarget = {
-   drop(props, monitor) {
-
-      // Obtain the dragged item
-      const item = monitor.getItem()
-
-      if (props.onDrop) {
-         props.onDrop(item, props.row)
-      }
-   },
-   canDrop(props, monitor) {
-      if (props.canDrop) {
-         return props.canDrop(props, monitor)
-      }
-      return false
-   }
-};
-
-const RowSource = {
-   canDrag(props, monitor) {
-      if (props.canDrag) {
-         return props.canDrag(props, monitor)
-      }
-      return false
-   },
-
-   beginDrag(props) {
-      return {
-         row: props.row,
-         type: props.type
-      }
-   }
-}
 
 
 const getItemStyle = (isDragging, draggableStyle) => ({
@@ -98,7 +64,6 @@ const getDefaultValue = (format) => {
 
 const rowFunctionComponent = (props) => {
    const {
-      canDrag,
       allowToDragRows = true,
       index: rowIndex,
       row,
@@ -129,8 +94,48 @@ const rowFunctionComponent = (props) => {
    useEffect(() => {
       setHasScrolled(false);
    }, [props.scrollTo]);
+      
 
-   useEffect(() => {
+   // Definimos los hooks useDrag y useDrop
+   const [{ isDragging, canDrag }, connectDragSource] = useDrag({
+      type: ItemTypes.ROW,
+      item: {
+        row: row,
+        type: props.type,
+      },
+      canDrag: (monitor) => {
+        return props.canDrag ? props.canDrag(props, monitor) : false;
+      },
+      item: () => {
+        return {
+          row: row,
+          type: props.type,
+        };
+      },
+      collect: (monitor) => ({
+        isDragging: monitor.isDragging(),
+        canDrag: props.canDrag ? props.canDrag(props, monitor) : false,
+      }),
+    });
+
+    const [{isOver, canDrop}, connectDropTarget] = useDrop({
+      accept: ItemTypes.ROW,
+      drop: (item) => {
+        if (props.onDrop) {
+          props.onDrop(item, row);
+        }
+      },
+      canDrop: (item, monitor) => {
+         const canBeDropped = props.canDrop ? props.canDrop(props, item) : false;
+        return canBeDropped
+      },
+      collect: (monitor) => ({
+        isOver: monitor.isOver({ shallow: true }),
+        canDrop: (props.canDrop && monitor.getItem()) ? props.canDrop(props, monitor.getItem()) : false,
+      }),
+    });
+
+    useEffect(() => {
       let classname = 'Table-Row';
       if (props.is_selected) {
          classname += ` Table-Row-Selected Table-Row--depth-${depth} ${row.is_item ? 'Table-Row-Item' : ''}`
@@ -148,54 +153,19 @@ const rowFunctionComponent = (props) => {
             classname += ` Table-Row--depth-${depth}`;
             classname += ignoreItemStyle ? ` Table-Row--depth-${depth}` : (`${row.is_item ? ` Table-Row-Item depth-${depth}` : ` Table-Row--depth-${depth}`}`);
          }
+
+         if (canDrop && isOver) {
+            classname += ' Table-Row-Over'
+         }
       }
 
       if (customRowClass) {
          classname += ' ' + customRowClass(row)
       }
-
-      setClassName(classname);      
-   }, [props.is_selected, styleTheme, rowIndex, row.is_item, depth, customRowClass]);
       
 
-   // Definimos los hooks useDrag y useDrop
-   const [{ isDragging }, connectDragSource] = useDrag({
-      type: ItemTypes.ROW,
-      item: {
-        row: row,
-        type: props.type,
-      },
-      canDrag: (monitor) => {
-        return props.canDrag ? props.canDrag(props, monitor) : false;
-      },
-      item: () => {
-        return {
-          row: row,
-          type: props.type,
-        };
-      },
-    });
-
-    const [collectedProps, connectDropTarget] = useDrop({
-      accept: ItemTypes.ROW,
-      drop: (item) => {
-        if (props.onDrop) {
-          props.onDrop(item, row);
-        }
-      },
-      canDrop: (item, monitor) => {
-         const canBeDropped = props.canDrop ? props.canDrop(props, item) : false;
-        return canBeDropped
-      },
-      hover: (item, monitor) => {
-         if (monitor.isOver({ shallow: true })) {
-            if (monitor.canDrop()) setClassName(`${className}${className.indexOf('Table-Row-Over') === -1 ? ' Table-Row-Over' : ''}`)
-         } else {
-            let classname = className.replace(' Table-Row-Over', '')
-            setClassName(classname)
-         }
-      },
-    });
+      setClassName(classname);      
+   }, [props.is_selected, styleTheme, rowIndex, row.is_item, depth, customRowClass, isOver, canDrop]);
 
    const {
       // cDP, 
