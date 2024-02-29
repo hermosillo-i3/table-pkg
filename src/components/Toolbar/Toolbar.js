0f71c56042d0febb30b38146ee6d50200bc251b4
Daniel Rodriguez <daniel.rodriguez@hermosillo.com>
2024-02-29 09:21:07 -0800

    fix

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -80,7 +80,6 @@ export default class Toolbar extends React.Component {
 
    getExcelRows = () => {
       const {rows = []} = this.props;
-      console.log(rows)
       const filteredRows = rows.map((row) => filterRowValues(row));
       return convertTreeStructureToFlatArray(filteredRows, 'subrows');
    }
